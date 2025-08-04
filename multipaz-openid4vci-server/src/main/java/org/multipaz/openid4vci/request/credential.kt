6cd06d50d243576cb5e19ab5a23c3e498c5c4bb9
Hanlu Feng <hanlu@openmobilehub.com>
2025-08-04 16:36:55 -0700

    revert credential.kt

--- multipaz-openid4vci-server/src/main/java/org/multipaz/openid4vci/request/credential.kt
+++ multipaz-openid4vci-server/src/main/java/org/multipaz/openid4vci/request/credential.kt
@@ -214,11 +214,10 @@ private const val TAG = "credential"
 private suspend fun readSystemOfRecord(state: IssuanceState): DataItem {
     val systemOfRecordAccess = state.systemOfRecordAccess!!
     val systemOfRecordUrl = BackendEnvironment.getSystemOfRecordUrl()
-    val (givenName, familyName, birthDate) = systemOfRecordAccess.accessToken.split(":")
-//    if (systemOfRecordUrl == null && givenName.trim().equals("John", ignoreCase = true) && familyName.trim().equals("Lee", ignoreCase = true) && birthDate == "09/04/1987") {
-    if (systemOfRecordUrl == null ) {
-
-// Running without System of Record (demo/dev mode) with expected demo data
+    if (systemOfRecordUrl == null) {
+        // Running without System of Record (demo/dev mode). Expect basic data encoded
+        // as fake access token
+        val (givenName, familyName, birthDate) = systemOfRecordAccess.accessToken.split(":")
         return buildCborMap {
             putCborMap("core") {
                 put("given_name", givenName)
